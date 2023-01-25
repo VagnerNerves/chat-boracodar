27042c6ef00fd92eb9859dae4b44ba66d92e913f
Vagner Nerves <vagnernervessantos@gmail.com>
2023-01-25 14:37:47 -0300

    style: Adjusted default color and font.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,25 @@
 module.exports = {
   content: ['./src/**/*.tsx', './index.html'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif']
+      },
+      colors: {
+        gray: {
+          50: '#E1E1E6'
+        },
+        green: {
+          600: '#00B37E',
+          700: '#07847E'
+        },
+        violet: {
+          500: '#633BBC',
+          800: '#282843',
+          900: '#1A1924'
+        }
+      }
+    }
   },
   plugins: []
 }
